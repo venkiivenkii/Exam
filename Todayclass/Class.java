bd79013e74c006aa8cf228367ce9904b0eadca19
venkiivenkii <vvenki090299@gmail.com>
2023-11-23 14:23:44 +0530

    hi

---
+++ Todayclass/Class.java
@@ -0,0 +1,21 @@
+package Todayclass;
+
+import java.util.Scanner;
+
+public class Class {
+	public static void main(String[] args) {
+		Scanner a=new Scanner(System.in);
+		
+		System.out.println("My wife name");
+		String next=a.next();
+		System.out.println(next);
+		
+		System.out.println("My Name ");
+		String nextline=a.nextLine();
+		System.out.println(nextline);
+		
+		
+	}
+
+
+}
